b8b60b8f18373b36ddeceebbd83aab625b2e04e7
Elie Khoury <elie.kh@gmail.com>
2020-06-22 15:11:42 +0200

    collecting tweets

--- index.js
+++ index.js
@@ -25,31 +25,50 @@ function search(callback) {
     );
 }
 
-function saveToJson(tweets){
-    var jsonData = JSON.stringify(tweets);
-    fs.writeFile("tweets.json", jsonData, function(err) {
+function saveToJson(tweets, index) {
+    var jsonData = JSON.stringify(tweets, null, 2);
+    fs.writeFile("tweets/tweets_" + index + ".json", jsonData, function (err) {
         if (err) {
             console.log(err);
         }
     });
 }
 
+function filter_stream(keywords) {
+    var stream = Twitter.stream("statuses/filter", { track: keywords });
+    console.info('Processing tweets ...');
+    var tweets = [];
+    var ids = [];
+    var index = 1;
 
+    if (!fs.existsSync("tweets")) {
+        fs.mkdirSync("tweets");
+    }
 
-function filter_stream(){
-    var stream = Twitter.stream("statuses/filter",{ track: ['stuttgart'] });
-    console.info('Awaiting for tweets ...');
-
-    stream.on('tweet', function(tweet){
-        var jsonData = JSON.stringify(tweet)+",";
-        fs.appendFile("tweets.json", jsonData, function(err) {
-            if (err) {
-                console.log(err);
+    stream.on('tweet', function (tweet_raw) {
+        var tweet = tweet_raw.text;
+        var id = tweet_raw.id;
+        if (tweet_raw.extended_tweet != undefined) {
+            tweet = tweet_raw.extended_tweet.full_text;
+        } else if (tweet_raw.retweeted_status != undefined) {
+            if (tweet_raw.retweeted_status.extended_tweet != null) {
+                tweet = tweet_raw.retweeted_status.extended_tweet.full_text;
+            } else {
+                tweet = tweet_raw.retweeted_status.text;
             }
-        });
+            id = tweet_raw.retweeted_status.id;
+        }
+        if (ids.indexOf(id) == -1) {
+            ids.push(id);
+            tweets.push(tweet);
+        }
+        if (tweets.length == 1000) {
+            saveToJson(tweets, index);
+            tweets = [];
+            index++;
+        }
     });
 }
 
 
-filter_stream();
-//search(saveToJson);
+filter_stream(['stuttgart','stgt2106']);
